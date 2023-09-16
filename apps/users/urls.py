8b5ef0ea76b8ec0c5d5a7344aab16908cec4647d
mr-ema <ema.a.v.c2017@gmail.com>
2023-09-15 23:55:54 -0300

    add logout url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
 from .views import (UserLoginView, UserRegistrationView, UserProfileView)
 
 app_name = 'users'
@@ -7,4 +9,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
