b503f648101cdc6434f7a88efa13234e03d0aa98
mr-ema <ema.a.v.c2017@gmail.com>
2023-09-20 19:22:32 -0300

    improve products styles and add pagination

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -7,4 +7,5 @@ app_name = 'products'
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
 ]
